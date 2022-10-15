2afb70eabf8059c7f38eb5b0c9714f129351c097
Abdullah67890 <abdullahmushtaq933@gmail.com>
2022-10-15 07:27:19 +0500

    todo App Prac

---
+++ public/app (2).js
@@ -0,0 +1,147 @@
+// // Slider Task
+
+// // var imgsParent = document.getElementById('imgsParent')
+// // var displayImage = document.getElementById('displayImage')
+// // var allImges = imgsParent.getElementsByTagName('IMG')
+
+// // function initSlider() {
+// //     displayImage.src = allImges[0].src  // indexNum
+// // }
+// // initSlider()
+
+// var display_image = document.getElementById('displayIMG')
+
+// var parent_image = document.getElementById('imageParent')
+// var all_images = parent_image.getElementsByTagName('img')
+
+// var indexNum = 0
+
+// function runSlider () {
+
+//     display_image.src = all_images[indexNum].src
+
+// }
+// runSlider()
+
+// // now buttons
+
+// function prev() {
+    
+//     if (indexNum == 0) {
+        
+//         indexNum = all_images.length
+        
+//     } 
+    
+//     else {
+//         indexNum--
+
+//         runSlider()
+//     }
+    
+// }
+
+
+// function next() {
+    
+//     if (indexNum == 0) {
+        
+//             indexNum = all_images.length-1
+//         }
+        
+//         else {
+            
+//                 indexNum++
+
+//                 runSlider()
+//             }         
+   
+
+// }
+
+
+
+// DOM  before
+
+
+// var parent = document.getElementById("parent").childNodes
+// console.log(parent[1].nodeType)
+// console.log(parent[1].nodeName)
+
+// function generateElement () {
+
+//     var p = document.createElement('P')
+//     var text = "Abdullah Bse20s000"
+//     var textNode = document.createTextNode(text)
+
+//     p.appendChild(textNode)
+
+//     console.log(p)
+
+//     var parent = document.getElementById('parents')
+//     parent.appendChild(p)
+
+// }
+
+// task
+
+// function generateText() {
+
+//     var p = document.createElement('P')
+//     // var txt = document.getElementById("pera").value
+//     var text = "ABACUS COMP"
+//     var txtNode = document.createTextNode(text)
+//     p.appendChild(txtNode)
+//     console.log(p)
+
+// }
+
+// var btn = document.createElement('BUTTON')
+// var btnText = "Delete"
+// btnText = document.createTextNode(btnText)
+// btn.appendChild(btnText)
+// btn.setAttribute('onClick' , 'del(this)')
+
+// p.appendChild(txtNode)
+
+// function deletePera(element) {
+
+//     element.parentNode.remove()
+
+//     cons
+
+
+// }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
